fd1d23976dd4930b9ea4dca3757c07f526c6497a
Mez Pahlan <mez.pahlan@gmail.com>
2019-07-20 18:47:49 +0100

    [JENKINS-58579] Add basic RoundTrip test

---
+++ src/test/java/io/jenkins/plugins/appcenter/RoundTripTest.java
@@ -0,0 +1,37 @@
+package io.jenkins.plugins.appcenter;
+
+import hudson.util.Secret;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class RoundTripTest {
+
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void should_Configure_AppCenterRecorder_With_Required_Inputs() throws Exception {
+        // Given
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder(
+            "api-token",
+            "owner-name",
+            "app-name",
+            "distribution-group",
+            "path/to/app.apk"
+        );
+
+        // When
+        j.configRoundtrip(appCenterRecorder);
+
+        // Then
+        assertThat(appCenterRecorder.getApiToken()).isEqualTo(Secret.fromString("api-token"));
+        assertThat(appCenterRecorder.getOwnerName()).isEqualTo("owner-name");
+        assertThat(appCenterRecorder.getAppName()).isEqualTo("app-name");
+        assertThat(appCenterRecorder.getDistributionGroups()).isEqualTo("distribution-group");
+        assertThat(appCenterRecorder.getPathToApp()).isEqualTo("path/to/app.apk");
+    }
+
+}
