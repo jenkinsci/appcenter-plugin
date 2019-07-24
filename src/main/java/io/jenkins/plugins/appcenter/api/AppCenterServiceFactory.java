27c589cc98c3a314f75532b6005950c4db5db329
gitolicious <mrjchn@gmail.com>
2019-07-24 20:58:40 +0000

    add proxy support for AppCenterService and UploadService

--- src/main/java/io/jenkins/plugins/appcenter/api/AppCenterServiceFactory.java
+++ src/main/java/io/jenkins/plugins/appcenter/api/AppCenterServiceFactory.java
@@ -1,6 +1,11 @@
 package io.jenkins.plugins.appcenter.api;
 
+import hudson.ProxyConfiguration;
+import hudson.model.TaskListener;
 import hudson.util.Secret;
+import jenkins.model.Jenkins;
+import okhttp3.Authenticator;
+import okhttp3.Credentials;
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -10,8 +15,16 @@ import retrofit2.converter.moshi.MoshiConverterFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.io.PrintStream;
 import java.io.Serializable;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URL;
+import java.util.Optional;
+
+import static com.google.common.base.Strings.nullToEmpty;
+import static java.net.Proxy.Type.HTTP;
+import static org.apache.commons.lang.StringUtils.isNotEmpty;
 
 public final class AppCenterServiceFactory implements Serializable {
 
@@ -22,9 +35,16 @@ public final class AppCenterServiceFactory implements Serializable {
     private final Secret apiToken;
     private final String baseUrl;
 
-    public AppCenterServiceFactory(@Nonnull Secret apiToken, @Nullable URL baseUrl) {
+    private final Optional<ProxyConfiguration> proxy;
+    private final TaskListener taskListener;
+
+    public AppCenterServiceFactory(@Nonnull Secret apiToken, @Nullable URL baseUrl, @Nonnull Jenkins jenkins, TaskListener taskListener) {
         this.apiToken = apiToken;
         this.baseUrl = baseUrl != null ? baseUrl.toString() : APPCENTER_BASE_URL;
+
+        proxy = Optional.ofNullable(jenkins).map(j -> j.proxy);
+
+        this.taskListener = taskListener;
     }
 
     public AppCenterService createAppCenterService() {
@@ -48,6 +68,8 @@ public final class AppCenterServiceFactory implements Serializable {
             return chain.proceed(newRequest);
         });
 
+        proxy.ifPresent(cfg -> setProxy(cfg, builder));
+
         final OkHttpClient okHttpClient = builder.build();
 
         final Retrofit retrofit = new Retrofit.Builder()
@@ -64,6 +86,8 @@ public final class AppCenterServiceFactory implements Serializable {
 
         final OkHttpClient.Builder builder = createHttpClientBuilder();
 
+        proxy.ifPresent(cfg -> setProxy(cfg, builder));
+        
         final OkHttpClient okHttpClient = builder.build();
 
         final Retrofit retrofit = new Retrofit.Builder()
@@ -86,5 +110,31 @@ public final class AppCenterServiceFactory implements Serializable {
 
     public Secret getApiToken() {
         return apiToken;
+    }
+
+    private void setProxy(
+        final ProxyConfiguration cfg,
+        final OkHttpClient.Builder builder) {
+
+        final PrintStream logger = taskListener.getLogger();
+        logger.println("Setting proxy.");
+        logger.println(cfg);
+
+        builder
+            .proxy(new Proxy(HTTP, new InetSocketAddress(cfg.name, cfg.port)));
+        final String username = nullToEmpty(cfg.getUserName());
+        final String password = nullToEmpty(cfg.getPassword());
+
+        if (isNotEmpty(username) && isNotEmpty(password)) {
+            final String credentials = Credentials.basic(username, password);
+
+            final Authenticator proxyAuthenticator = (route, response) -> response
+                .request()
+                .newBuilder()
+                .header("Proxy-Authorization", credentials)
+                .build();
+
+            builder.proxyAuthenticator(proxyAuthenticator);
+        }
     }
 }
