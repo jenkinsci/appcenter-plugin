8c9826bba0feec4125984e29d3848b810d912e8f
gitolicious <mrjchn@gmail.com>
2019-08-04 16:15:24 +0200

    improved test coverage for proxy tests code cleanup

--- src/test/java/io/jenkins/plugins/appcenter/ProxyTest.java
+++ src/test/java/io/jenkins/plugins/appcenter/ProxyTest.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.appcenter;
 
+import com.google.common.net.HttpHeaders;
 import hudson.Launcher;
 import hudson.ProxyConfiguration;
 import hudson.model.AbstractBuild;
@@ -7,6 +8,8 @@ import hudson.model.BuildListener;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
+import io.jenkins.plugins.appcenter.api.AppCenterServiceFactory;
+import okhttp3.Credentials;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Before;
@@ -18,6 +21,8 @@ import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.TestBuilder;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Objects;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -51,6 +56,26 @@ public class ProxyTest {
     }
 
     @Test
+    public void should_SendRequestsDirectly_When_NoProxyConfigurationFound() throws Exception {
+        // Given
+        jenkinsRule.jenkins.proxy = null;
+
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("token", "owner_name", "app_name", "Collaborators", "path/to/app.apk");
+        appCenterRecorder.setBaseUrl(mockWebServer.url("/").url()); // Notice this is *not* set to the proxy address
+
+        enqueueRequests(mockWebServer);
+        freeStyleProject.getPublishersList().add(appCenterRecorder);
+
+        // When
+        final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
+
+        // Then
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        assertThat(proxyWebServer.getRequestCount()).isEqualTo(0);
+        assertThat(mockWebServer.getRequestCount()).isEqualTo(4);
+    }
+
+    @Test
     public void should_SendRequestsToProxy_When_ProxyConfigurationFound() throws Exception {
         // Given
         jenkinsRule.jenkins.proxy = new ProxyConfiguration(proxyWebServer.getHostName(), proxyWebServer.getPort());
@@ -58,21 +83,7 @@ public class ProxyTest {
         final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("token", "owner_name", "app_name", "Collaborators", "path/to/app.apk");
         appCenterRecorder.setBaseUrl(mockWebServer.url("/").url()); // Notice this is *not* set to the proxy address
 
-        proxyWebServer.enqueue(new MockResponse().setResponseCode(201).setBody("{\n" +
-            "  \"upload_id\": \"string\",\n" +
-            "  \"upload_url\": \"" + mockWebServer.url("/").toString() + "\",\n" +
-            "  \"asset_id\": \"string\",\n" +
-            "  \"asset_domain\": \"string\",\n" +
-            "  \"asset_token\": \"string\"\n" +
-            "}"));
-        proxyWebServer.enqueue(new MockResponse().setResponseCode(200));
-        proxyWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("{\n" +
-            "  \"release_id\": 0,\n" +
-            "  \"release_url\": \"string\"\n" +
-            "}"));
-        proxyWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("{\n" +
-            "  \"release_notes\": \"string\"\n" +
-            "}"));
+        enqueueRequests(proxyWebServer);
         freeStyleProject.getPublishersList().add(appCenterRecorder);
 
         // When
@@ -82,5 +93,100 @@ public class ProxyTest {
         jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
         assertThat(proxyWebServer.getRequestCount()).isEqualTo(4);
         assertThat(mockWebServer.getRequestCount()).isEqualTo(0);
+    }
+
+
+    @Test
+    public void should_SendProxyAuthorizationHeader_When_ProxyCredentialsConfigured() throws Exception {
+        // Given
+        final String userName = "user";
+        final String password = "password";
+
+        jenkinsRule.jenkins.proxy = new ProxyConfiguration(proxyWebServer.getHostName(), proxyWebServer.getPort(), userName, password);
+
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("token", "owner_name", "app_name", "Collaborators", "path/to/app.apk");
+        appCenterRecorder.setBaseUrl(mockWebServer.url("/").url()); // Notice this is *not* set to the proxy address
+
+        // first request rejected and proxy authentication requested
+        proxyWebServer.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_PROXY_AUTH));
+        enqueueRequests(proxyWebServer);
+        freeStyleProject.getPublishersList().add(appCenterRecorder);
+
+        // When
+        final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
+
+        // Then
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        assertThat(proxyWebServer.getRequestCount()).isEqualTo(5);
+        assertThat(mockWebServer.getRequestCount()).isEqualTo(0);
+        // proxy auth is performed on second request
+        assertThat(proxyWebServer.takeRequest().getHeader(HttpHeaders.PROXY_AUTHORIZATION)).isNull();
+        assertThat(proxyWebServer.takeRequest().getHeader(HttpHeaders.PROXY_AUTHORIZATION)).isEqualTo(Credentials.basic(userName, password));
+    }
+
+    @Test
+    public void should_SendAllRequestsDirectly_When_NoProxyHostConfigured() throws Exception {
+        // Given
+        final String noProxyHost = String.join(",", new URL(AppCenterServiceFactory.APPCENTER_BASE_URL).getHost(), mockWebServer.url("/").url().getHost());
+
+        jenkinsRule.jenkins.proxy = new ProxyConfiguration(proxyWebServer.getHostName(), proxyWebServer.getPort(), null, null, noProxyHost);
+
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("token", "owner_name", "app_name", "Collaborators", "path/to/app.apk");
+        appCenterRecorder.setBaseUrl(mockWebServer.url("/").url()); // Notice this is *not* set to the proxy address
+
+        enqueueRequests(mockWebServer);
+        freeStyleProject.getPublishersList().add(appCenterRecorder);
+
+        // When
+        final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
+
+        // Then
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        assertThat(proxyWebServer.getRequestCount()).isEqualTo(0);
+        assertThat(mockWebServer.getRequestCount()).isEqualTo(4);
+    }
+
+    @Test
+    public void should_SendUploadRequestsDirectly_When_NoProxyHostConfiguredForAppCenterAPI() throws Exception {
+        // Given
+        final String noProxyHost = new URL(AppCenterServiceFactory.APPCENTER_BASE_URL).getHost();
+
+        jenkinsRule.jenkins.proxy = new ProxyConfiguration(proxyWebServer.getHostName(), proxyWebServer.getPort(), null, null, noProxyHost);
+
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("token", "owner_name", "app_name", "Collaborators", "path/to/app.apk");
+        appCenterRecorder.setBaseUrl(mockWebServer.url("/").url()); // Notice this is *not* set to the proxy address
+
+        enqueueRequests(mockWebServer, proxyWebServer);
+        freeStyleProject.getPublishersList().add(appCenterRecorder);
+
+        // When
+        final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
+
+        // Then
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        assertThat(proxyWebServer.getRequestCount()).isEqualTo(1);
+        assertThat(mockWebServer.getRequestCount()).isEqualTo(3);
+    }
+
+    private void enqueueRequests(MockWebServer mockWebServer) {
+        enqueueRequests(mockWebServer, mockWebServer);
+    }
+
+    private void enqueueRequests(MockWebServer mockWebServer, MockWebServer proxyWebServer) {
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_CREATED).setBody("{\n" +
+            "  \"upload_id\": \"string\",\n" +
+            "  \"upload_url\": \"" + proxyWebServer.url("/").toString() + "\",\n" +
+            "  \"asset_id\": \"string\",\n" +
+            "  \"asset_domain\": \"string\",\n" +
+            "  \"asset_token\": \"string\"\n" +
+            "}"));
+        proxyWebServer.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody("{\n" +
+            "  \"release_id\": 0,\n" +
+            "  \"release_url\": \"string\"\n" +
+            "}"));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody("{\n" +
+            "  \"release_notes\": \"string\"\n" +
+            "}"));
     }
 }
