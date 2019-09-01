a65920c2e22d4e14e93ec7cce866e80429f19596
Mez Pahlan <mez.pahlan@gmail.com>
2019-09-01 19:58:53 +0100

    [JENKINS-58887] Add CommitUploadResource internal task

---
+++ src/test/java/io/jenkins/plugins/appcenter/task/internal/CommitUploadResourceTaskTest.java
@@ -0,0 +1,85 @@
+package io.jenkins.plugins.appcenter.task.internal;
+
+import hudson.ProxyConfiguration;
+import hudson.model.TaskListener;
+import hudson.util.Secret;
+import io.jenkins.plugins.appcenter.AppCenterException;
+import io.jenkins.plugins.appcenter.api.AppCenterServiceFactory;
+import io.jenkins.plugins.appcenter.model.appcenter.ReleaseUploadEndResponse;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import retrofit2.HttpException;
+
+import java.io.PrintStream;
+import java.util.concurrent.ExecutionException;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CommitUploadResourceTaskTest {
+
+    @Rule
+    public MockWebServer mockWebServer = new MockWebServer();
+
+    @Mock
+    TaskListener mockTaskListener;
+
+    @Mock
+    PrintStream mockLogger;
+
+    @Mock
+    ProxyConfiguration mockProxyConfig;
+
+    private CommitUploadResourceTask task;
+
+    @Before
+    public void setUp() {
+        given(mockTaskListener.getLogger()).willReturn(mockLogger);
+        final AppCenterServiceFactory factory = new AppCenterServiceFactory(Secret.fromString("secret-token"), mockWebServer.url("/").toString(), mockProxyConfig);
+        task = new CommitUploadResourceTask(mockTaskListener, factory);
+    }
+
+    @Test
+    public void should_ReturnResponse_When_RequestIsSuccessful() throws Exception {
+        // Given
+        final CommitUploadResourceTask.Request request = new CommitUploadResourceTask.Request("owner-name", "app-name", "upload-id");
+        final ReleaseUploadEndResponse expected = new ReleaseUploadEndResponse(0, "string");
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("{\n" +
+            "  \"release_id\": 0,\n" +
+            "  \"release_url\": \"string\"\n" +
+            "}"));
+
+        // When
+        final ReleaseUploadEndResponse actual = task.execute(request).get();
+
+        // Then
+        assertThat(actual)
+            .isEqualTo(expected);
+    }
+
+    @Test
+    public void should_ReturnException_When_RequestIsUnSuccessful() throws Exception {
+        // Given
+        final CommitUploadResourceTask.Request request = new CommitUploadResourceTask.Request("owner-name", "app-name", "upload-id");
+        mockWebServer.enqueue(new MockResponse().setResponseCode(400));
+
+        // When
+        final ThrowingRunnable throwingRunnable = () -> task.execute(request).get();
+
+        // Then
+        final ExecutionException exception = assertThrows(ExecutionException.class, throwingRunnable);
+        assertThat(exception).hasCauseThat().isInstanceOf(AppCenterException.class);
+        assertThat(exception).hasCauseThat().hasMessageThat().isEqualTo("Committing resource unsuccessful: ");
+        assertThat(exception).hasCauseThat().hasCauseThat().isInstanceOf(HttpException.class);
+        assertThat(exception).hasCauseThat().hasCauseThat().hasMessageThat().isEqualTo("HTTP 400 Client Error");
+    }
+}
