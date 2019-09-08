46c64d2dece56d9c7d98984ef5545d5cfa5d7a92
Mez Pahlan <mez.pahlan@gmail.com>
2019-09-08 21:54:30 +0100

    [JENKINS-58887] Rename module

--- src/main/java/io/jenkins/plugins/appcenter/di/AuthModule.java
+++ src/main/java/io/jenkins/plugins/appcenter/di/AuthModule.java
@@ -5,22 +5,13 @@ import dagger.Provides;
 import hudson.util.Secret;
 import io.jenkins.plugins.appcenter.AppCenterRecorder;
 
-import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Module
-final class ApiModule {
-
+final class AuthModule {
     @Provides
     @Singleton
     static Secret provideApiToken(AppCenterRecorder appCenterRecorder) {
         return appCenterRecorder.getApiToken();
-    }
-
-    @Provides
-    @Singleton
-    @Named("baseUrl")
-    static String provideBaseUrl() {
-        return "https://api.appcenter.ms/";
     }
 }
