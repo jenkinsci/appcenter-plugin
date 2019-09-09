631c8c89cba7e5215e52aeca1b2fb60996d4352c
Mez Pahlan <mez.pahlan@gmail.com>
2019-09-09 21:48:04 +0100

    [JENKINS-58887] Use HttpURLConnection constants

--- src/test/java/io/jenkins/plugins/appcenter/api/MockWebServerUtil.java
+++ src/test/java/io/jenkins/plugins/appcenter/api/MockWebServerUtil.java
@@ -5,27 +5,31 @@ import okhttp3.mockwebserver.MockWebServer;
 
 import javax.annotation.Nonnull;
 
+import static java.net.HttpURLConnection.HTTP_CREATED;
+import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR;
+import static java.net.HttpURLConnection.HTTP_OK;
+
 public class MockWebServerUtil {
 
     public static void success(final @Nonnull MockWebServer mockWebServer) {
-        mockWebServer.enqueue(new MockResponse().setResponseCode(201).setBody("{\n" +
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HTTP_CREATED).setBody("{\n" +
             "  \"upload_id\": \"string\",\n" +
             "  \"upload_url\": \"" + mockWebServer.url("/").toString() + "\",\n" +
             "  \"asset_id\": \"string\",\n" +
             "  \"asset_domain\": \"string\",\n" +
             "  \"asset_token\": \"string\"\n" +
             "}"));
-        mockWebServer.enqueue(new MockResponse().setResponseCode(200));
-        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("{\n" +
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HTTP_OK));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HTTP_OK).setBody("{\n" +
             "  \"release_id\": 0,\n" +
             "  \"release_url\": \"string\"\n" +
             "}"));
-        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("{\n" +
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HTTP_OK).setBody("{\n" +
             "  \"release_notes\": \"string\"\n" +
             "}"));
     }
 
     public static void failure(final @Nonnull MockWebServer mockWebServer) {
-        mockWebServer.enqueue(new MockResponse().setResponseCode(500));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(HTTP_INTERNAL_ERROR));
     }
 }
