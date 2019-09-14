36bafe3a6a3b86c595d3c435a9982996c435a419
Mez Pahlan <mez.pahlan@gmail.com>
2019-09-14 18:08:32 +0100

    [JENKINS-58579] Add ConfigurationTest

---
+++ src/test/java/io/jenkins/plugins/appcenter/ConfigurationTest.java
@@ -0,0 +1,37 @@
+package io.jenkins.plugins.appcenter;
+
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import hudson.model.FreeStyleProject;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class ConfigurationTest {
+
+    @ClassRule
+    public static JenkinsRule jenkinsRule = new JenkinsRule();
+
+    private FreeStyleProject freeStyleProject;
+
+    @Before
+    public void setUp() throws Exception {
+        freeStyleProject = jenkinsRule.createFreeStyleProject();
+    }
+
+    @Test
+    public void should_Configure_RequiredParameters_ViaWebForm() throws Exception {
+        // Given
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("at-this-moment-you-should-be-with-us", "janes-addiction", "ritual-de-lo-habitual", "three/days/xiola.apk", "casey, niccoli");
+        freeStyleProject.getPublishersList().add(appCenterRecorder);
+
+        final HtmlForm htmlForm = jenkinsRule.createWebClient().getPage(freeStyleProject, "configure").getFormByName("config");
+        jenkinsRule.submit(htmlForm);
+
+        // When
+        final AppCenterRecorder configuredAppCenterRecorder = freeStyleProject.getPublishersList().get(AppCenterRecorder.class);
+
+        // Then
+        jenkinsRule.assertEqualDataBoundBeans(appCenterRecorder, configuredAppCenterRecorder);
+    }
+}
