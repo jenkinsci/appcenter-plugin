16ac61607a992046c24a408b8e3f17d6f3c2a693
Balthazar Frolin <balthazarfrolin@gmail.com>
2019-09-14 23:35:03 +0200

    [JENKINS-59312] Enable variable in OwnerName, AppName and DistributionGroup configuration (#21)
    * Allow variable in OwnerName, AppName and DistributionGroup configuration
    
    * [JENKINS-59312] Expand environment variables for all upload parameters
    
    * [JENKINS-59312] Clean up environment variable tests
    
    * [JENKINS-59312] Assert content received at API
    
    * [JENKINS-59312] Add test for AppName EnvVar interpolation
    
    * [JENKINS-59312] Add test for DestinationGroups EnvVar interpolation

--- src/test/java/io/jenkins/plugins/appcenter/EnvInterpolationTest.java
+++ src/test/java/io/jenkins/plugins/appcenter/EnvInterpolationTest.java
@@ -1,11 +1,14 @@
 package io.jenkins.plugins.appcenter;
 
+import hudson.EnvVars;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
 import io.jenkins.plugins.appcenter.util.MockWebServerUtil;
 import io.jenkins.plugins.appcenter.util.TestUtil;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -13,6 +16,8 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import java.io.IOException;
+
+import static com.google.common.truth.Truth.assertThat;
 
 public class EnvInterpolationTest {
 
@@ -27,40 +32,86 @@ public class EnvInterpolationTest {
     @Before
     public void setUp() throws IOException {
         freeStyleProject = jenkinsRule.createFreeStyleProject();
-        freeStyleProject.getBuildersList().add(TestUtil.createFileForFreeStyle("path/to/app-42.apk"));
+        freeStyleProject.getBuildersList().add(TestUtil.createFileForFreeStyle("three/days/xiola.apk"));
+
+        final EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
+        final EnvVars envVars = prop.getEnvVars();
+        envVars.put("OWNER_NAME", "janes-addiction");
+        envVars.put("APP_NAME", "ritual-de-lo-habitual");
+        envVars.put("PATH_TO_APP", "three/days/xiola.apk");
+        envVars.put("DISTRIBUTION_GROUPS", "casey, niccoli");
+
+        jenkinsRule.jenkins.getGlobalNodeProperties().add(prop);
+
+        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder(
+            "at-this-moment-you-should-be-with-us",
+            "${OWNER_NAME}",
+            "${APP_NAME}",
+            "${PATH_TO_APP}",
+            "${DISTRIBUTION_GROUPS}"
+        );
+        appCenterRecorder.setBaseUrl(mockWebServer.url("/").toString());
+
+        freeStyleProject.getPublishersList().add(appCenterRecorder);
     }
 
     @Test
-    public void should_InterpolateEnv_InAppPath() throws Exception {
+    public void should_InterpolateEnv_InOwnerName() throws Exception {
         // Given
         MockWebServerUtil.enqueueSuccess(mockWebServer);
-        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("at-this-moment-you-should-be-with-us", "janes-addiction", "ritual-de-lo-habitual", "path/to/app-${BUILD_NUMBER}.apk", "casey, niccoli");
-        appCenterRecorder.setBaseUrl(mockWebServer.url("/").toString());
-        freeStyleProject.getPublishersList().add(appCenterRecorder);
-
-        freeStyleProject.updateNextBuildNumber(42);
 
         // When
         final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
 
         // Then
         jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        final RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        assertThat(recordedRequest.getPath()).contains("janes-addiction");
     }
 
     @Test
-    public void should_LeaveUnchangedWhenNotInEnv_InAppPath() throws Exception {
+    public void should_InterpolateEnv_InAppName() throws Exception {
         // Given
-        final AppCenterRecorder appCenterRecorder = new AppCenterRecorder("at-this-moment-you-should-be-with-us", "janes-addiction", "ritual-de-lo-habitual", "path/to/app-${NOTINENV}.apk", "casey, niccoli");
-        appCenterRecorder.setBaseUrl(mockWebServer.url("/").url().toString());
-        freeStyleProject.getPublishersList().add(appCenterRecorder);
-
-        freeStyleProject.updateNextBuildNumber(42);
+        MockWebServerUtil.enqueueSuccess(mockWebServer);
 
         // When
         final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
 
         // Then
-        jenkinsRule.assertBuildStatus(Result.FAILURE, freeStyleBuild);
-        jenkinsRule.assertLogContains("File not found: path/to/app-${NOTINENV}.apk", freeStyleBuild);
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        final RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        assertThat(recordedRequest.getPath()).contains("ritual-de-lo-habitual");
+    }
+
+    @Test
+    public void should_InterpolateEnv_InAppPath() throws Exception {
+        // Given
+        MockWebServerUtil.enqueueSuccess(mockWebServer);
+
+        // When
+        final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
+
+        // Then
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        mockWebServer.takeRequest();
+        final RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        assertThat(recordedRequest.getBody().readUtf8()).contains("xiola.apk");
+    }
+
+    @Test
+    public void should_InterpolateEnv_InDestinationGroups() throws Exception {
+        // Given
+        MockWebServerUtil.enqueueSuccess(mockWebServer);
+
+        // When
+        final FreeStyleBuild freeStyleBuild = freeStyleProject.scheduleBuild2(0).get();
+
+        // Then
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, freeStyleBuild);
+        mockWebServer.takeRequest();
+        mockWebServer.takeRequest();
+        mockWebServer.takeRequest();
+        final RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        assertThat(recordedRequest.getBody().readUtf8()).contains("[{\"name\":\"casey\"},{\"name\":\"niccoli\"}]");
     }
 }
