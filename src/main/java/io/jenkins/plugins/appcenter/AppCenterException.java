15a16422bcece0ff95f64fc07aef9080aec75c8e
Mez Pahlan <mez.pahlan@gmail.com>
2019-10-26 16:51:03 +0100

    [JENKINS-59944] Remove unused exception constructors

--- src/main/java/io/jenkins/plugins/appcenter/AppCenterException.java
+++ src/main/java/io/jenkins/plugins/appcenter/AppCenterException.java
@@ -2,23 +2,11 @@ package io.jenkins.plugins.appcenter;
 
 public final class AppCenterException extends Exception {
 
-    public AppCenterException() {
-        super();
-    }
-
     public AppCenterException(String s) {
         super(s);
     }
 
     public AppCenterException(String s, Throwable throwable) {
         super(s, throwable);
-    }
-
-    public AppCenterException(Throwable throwable) {
-        super(throwable);
-    }
-
-    public AppCenterException(String s, Throwable throwable, boolean b, boolean b1) {
-        super(s, throwable, b, b1);
     }
 }
