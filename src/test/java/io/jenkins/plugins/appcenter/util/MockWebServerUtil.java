12340a7ac143587b428a5ec4e0564cb0c375318e
Mez Pahlan <mez.pahlan@gmail.com>
2020-03-18 21:58:41 +0000

    [JENKINS-59462] Add mockwebserver util method for symbol uploads

--- src/test/java/io/jenkins/plugins/appcenter/util/MockWebServerUtil.java
+++ src/test/java/io/jenkins/plugins/appcenter/util/MockWebServerUtil.java
@@ -42,6 +42,63 @@ public class MockWebServerUtil {
             "}"));
     }
 
+    public static void enqueueSuccessWithSymbols(final @Nonnull MockWebServer mockAppCenterServer) {
+        // Create upload resource for app
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_CREATED).setBody("{\n" +
+            "  \"upload_id\": \"string\",\n" +
+            "  \"upload_url\": \"" + mockAppCenterServer.url("/").toString() + "\",\n" +
+            "  \"asset_id\": \"string\",\n" +
+            "  \"asset_domain\": \"string\",\n" +
+            "  \"asset_token\": \"string\"\n" +
+            "}"));
+
+        // Create upload resource for debug symbols
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_CREATED).setBody("{\n" +
+            "  \"symbol_upload_id\": \"string\",\n" +
+            "  \"upload_url\": \"" + mockAppCenterServer.url("/").toString() + "\",\n" +
+            "  \"expiration_date\": \"2020-03-18T21:16:22.188Z\"\n" +
+            "}"));
+
+        // Upload app
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_OK));
+
+        // Upload debug symbols
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_OK));
+
+        // Commit app
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_OK).setBody("{\n" +
+            "  \"release_id\": 0,\n" +
+            "  \"release_url\": \"string\"\n" +
+            "}"));
+
+        // Commit debug symbols
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_OK).setBody("{\n" +
+            "  \"symbol_upload_id\": \"string\",\n" +
+            "  \"app_id\": \"string\",\n" +
+            "  \"user\": {\n" +
+            "    \"email\": \"string\",\n" +
+            "    \"display_name\": \"string\"\n" +
+            "  },\n" +
+            "  \"status\": \"created\",\n" +
+            "  \"symbol_type\": \"Apple\",\n" +
+            "  \"symbols_uploaded\": [\n" +
+            "    {\n" +
+            "      \"symbol_id\": \"string\",\n" +
+            "      \"platform\": \"string\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"origin\": \"User\",\n" +
+            "  \"file_name\": \"string\",\n" +
+            "  \"file_size\": 0,\n" +
+            "  \"timestamp\": \"2020-03-18T21:28:19.024Z\"\n" +
+            "}"));
+
+        // Distribute app
+        mockAppCenterServer.enqueue(new MockResponse().setResponseCode(HTTP_OK).setBody("{\n" +
+            "  \"release_notes\": \"string\"\n" +
+            "}"));
+    }
+
     public static void enqueueFailure(final @Nonnull MockWebServer mockWebServer) {
         mockWebServer.enqueue(new MockResponse().setResponseCode(HTTP_INTERNAL_ERROR));
     }
